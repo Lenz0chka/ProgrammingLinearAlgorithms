f6b95ab3dc301abbba5f8ae8746d6a0176fa52f5
Lenz0chka <leenzery@gmail.com>
2025-09-16 22:05:32 +0500

    Added code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Введите число для перевода из двоичной системы в десятичную");
+            string BinToDec = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(BinToDec, 2));
+
+            // Задание 2
+            Console.WriteLine("Введите число для перевода из десятичной системы в двоичную");
+            int DecToBin = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(DecToBin, 2));
+
+            //Задание 3
+            Console.WriteLine("Введите во сколько N часов была заказана доставка");
+            int N = int.Parse(Console.ReadLine()); // Часы
+            Console.WriteLine("Введите во сколько M минут была заказана доставка");
+            int M = int.Parse(Console.ReadLine()); // Минуты
+            Console.WriteLine("Введите через сколько T минут будет осуществлена доставка");
+            int T = int.Parse(Console.ReadLine()); // Продукты привезут через столько-то минут
+            Console.WriteLine($"На электронных часах будет {Convert.ToDateTime($"{N}:{M}").AddMinutes(T).ToShortTimeString().ToString()}, когда привезут долгожданные продукты");
+        }
+    }
+}
